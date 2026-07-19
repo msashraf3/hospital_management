da34410e6bfc259f499fa1c61462979f476db361
msashraf3 <mshamimas@proton.me>
2026-07-19 17:03:41 +0600

    code is formatted for the better view

--- hospital_management/models/doctor.py
+++ hospital_management/models/doctor.py
@@ -1,15 +1,20 @@
 from odoo import models, fields
 
-class HospitalDoctor(models.Model):
-    _name="hospital.doctor"
-    _description="Hospital Doctor"
 
-    name=fields.Char(string="Doctor Name",  required=True)
-    age=fields.Integer(string="Age")
-    gender=fields.Selection([('male','Male'), ('female', 'Female'),('others','Others')], string="Gender")
-    specialization=fields.Char(string="Specialization")
-    department=fields.Char(string="Department")
-    date_of_birth=fields.Date(string="Date of Birth")
-    phone=fields.Char(string="Phone Number")
-    email=fields.Char(string="E-mail Address")
-    appointment_ids=fields.One2many('hospital.appointment', 'doctor_id', string="Appointments")
+class HospitalDoctor(models.Model):
+    _name = "hospital.doctor"
+    _description = "Hospital Doctor"
+
+    name = fields.Char(string="Doctor Name", required=True)
+    age = fields.Integer(string="Age")
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female"), ("others", "Others")], string="Gender"
+    )
+    specialization = fields.Char(string="Specialization")
+    department = fields.Char(string="Department")
+    date_of_birth = fields.Date(string="Date of Birth")
+    phone = fields.Char(string="Phone Number")
+    email = fields.Char(string="E-mail Address")
+    appointment_ids = fields.One2many(
+        "hospital.appointment", "doctor_id", string="Appointments"
+    )
--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -3,14 +3,18 @@ from odoo import models, fields
 
 class HospitalPatient(models.Model):
 
-	_name = "hospital.patient"
-	_description = "Hospital Patient"
-	
-	name = fields.Char(string="Patient Name", required=True)
-	age=fields.Integer(string="Age")
-	gender=fields.Selection([('male','Male'),('female','Female'), ('others', 'Others')], string="Gender")
-	doctor_id=fields.Many2one('hospital.doctor', string="Assigned Doctor")
-	medical_history=fields.Text(string="Previous Medical History")
-	phone=fields.Char(string="Phone number")
-	date_of_birth=fields.Date(string="Date Of Birth")
-	appointment_ids=fields.One2many('hospital.appointment', 'patient_id', string="Appointments")
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string="Patient Name", required=True)
+    age = fields.Integer(string="Age")
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female"), ("others", "Others")], string="Gender"
+    )
+    doctor_id = fields.Many2one("hospital.doctor", string="Assigned Doctor")
+    medical_history = fields.Text(string="Previous Medical History")
+    phone = fields.Char(string="Phone number")
+    date_of_birth = fields.Date(string="Date Of Birth")
+    appointment_ids = fields.One2many(
+        "hospital.appointment", "patient_id", string="Appointments"
+    )
