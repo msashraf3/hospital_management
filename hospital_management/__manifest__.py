0d2bc52c6fa8b6aba02dbe2891a4e2408e7d579a
msashraf3 <mshamimas@proton.me>
2026-07-23 12:20:34 +0600

    Added hospital user groups (Doctor, Receptionist, Admin) with per-group access rights

--- hospital_management/__manifest__.py
+++ hospital_management/__manifest__.py
@@ -10,6 +10,7 @@
     """,
     # data files always loaded at installation
     "data": [
+        "security/hospital_security.xml",
         "security/ir.model.access.csv",
         "views/doctor_views.xml",
         "views/patient_views.xml",
